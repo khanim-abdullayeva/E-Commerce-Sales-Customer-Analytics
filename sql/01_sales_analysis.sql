5c8241f6013735cf61345c402957a9308ee00087
Khanim Abdullayeva <xabdullayeva775@gmail.com>
2026-09-10 17:41:15 +0400

    Update notebook

--- sql/01_sales_analysis.sql
+++ sql/01_sales_analysis.sql
@@ -1,12 +1,22 @@
-with Joined1 as(
-select s.*,
-replace(replace("Unit Price USD", "$",""), ",","") - replace(replace("Unit Cost USD", "$",""), ",","") as "Gross Profit USD",
+CREATE TEMP TABLE sales_analysis AS
+SELECT s.*,
+REPLACE(REPLACE("Unit Price USD", "$",""), ",","") - REPLACE(REPLACE("Unit Cost USD", "$",""), ",","") AS "Gross Profit USD",
 p.Category
-from sales s
-left join products p
-on s.ProductKey = p.ProductKey)
+FROM sales s
+LEFT JOIN products p
+ON s.ProductKey = p.ProductKey
 
-select *
-from Joined1
+SELECT *, Quantity * "Gross Profit USD" AS "Total Profit USD"
+FROM sales_analysis
+ORDER BY "Total Profit USD" DESC
+/* The order number that generated the highest profit was 433005 with 21397.7$ total profit 
+and the category of this product was Home Apliances
+*/
+
+SELECT Category, SUM(Quantity * "Gross Profit USD") AS "Total Profit USD"
+FROM sales_analysis
+GROUP BY Category
+ORDER BY "Total Profit USD" DESC
+
 
 
